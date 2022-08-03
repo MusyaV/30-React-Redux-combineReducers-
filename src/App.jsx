4a2209e6eda80d7672fa461d49071e0010eddd7c
MusyaV <C:\Users\parkinn\AppData\Roaming\The Bat!>
2022-08-03 21:25:19 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+
+function App() {
+
+  const dispatch = useDispatch();
+
+  const cash = useSelector(state => state.cash)
+
+  const addCash = (cash) => {
+    dispatch({type:'ADD_CASH', payload: cash})
+  }
+  const getCash = (cash) => {
+    dispatch({type:'GET_CASH', payload: cash})
+  }
+
+  return (
+    <div className={'app'}>
+      <div style={{ display: 'flex', marginTop: '10px' }}>
+        <div style={{ fontSize: '2rem', marginRight: '10px' }}>{cash}</div>
+        <button onClick={() => addCash(Number(prompt()))}>Add Cash</button>
+        <button onClick={() => getCash(Number(prompt()))}>Get Cash</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
